e9265ac7ba3338a3a9677f41f9ef3e3a7cff9874
Nurbek Normatov <nurbeknormatov729@gmail.com>
2022-12-05 05:02:58 +0500

    change main img

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 const toggleSidebar = () => document.body.classList.toggle("open");
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 10) {
+    $("#navbar").addClass("floatingNav");
+  } else {
+    $("#navbar").removeClass("floatingNav");
+  }
+});
